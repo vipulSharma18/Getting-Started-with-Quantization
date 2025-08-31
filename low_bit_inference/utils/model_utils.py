90705d4b407528f50941fd8185877206ad6df386
Vipul Sharma <vipuls181999@gmail.com>
2025-08-31 01:23:35 +0000

    organize rms norm kernels better

--- low_bit_inference/utils/model_utils.py
+++ low_bit_inference/utils/model_utils.py
@@ -3,12 +3,11 @@ from typing import Callable, Optional, Union
 import torch
 from torch import nn
 
-from ..optims.liger_rms_norm import LigerRMSNorm
+from ..optims.rms_norm_kernels.liger_rms_norm import LigerRMSNorm
 
 from ...activations import ACT2FN
 from ...cache_utils import Cache, DynamicCache
 from ...generation import GenerationMixin
-from ...integrations import use_kernel_forward_from_hub
 from ...masking_utils import create_causal_mask
 from ...modeling_layers import (
     GradientCheckpointingLayer,
@@ -27,24 +26,24 @@ from .configuration_llama import LlamaConfig
 
 # the hf hub kernel uses liger-kernel rms norm which we've directly imported in the code
 # @use_kernel_forward_from_hub("RMSNorm")
-class LlamaRMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
-        """
-        LlamaRMSNorm is equivalent to T5LayerNorm
-        """
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = eps
+# class LlamaRMSNorm(nn.Module):
+#     def __init__(self, hidden_size, eps=1e-6):
+#         """
+#         LlamaRMSNorm is equivalent to T5LayerNorm
+#         """
+#         super().__init__()
+#         self.weight = nn.Parameter(torch.ones(hidden_size))
+#         self.variance_epsilon = eps
 
-    def forward(self, hidden_states):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
+#     def forward(self, hidden_states):
+#         input_dtype = hidden_states.dtype
+#         hidden_states = hidden_states.to(torch.float32)
+#         variance = hidden_states.pow(2).mean(-1, keepdim=True)
+#         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+#         return self.weight * hidden_states.to(input_dtype)
 
-    def extra_repr(self):
-        return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
+#     def extra_repr(self):
+#         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
 
 
 class LlamaRotaryEmbedding(nn.Module):
