f226e67dcb45785fe43f99a4cc320f141e726da8
Vipul Sharma <vipuls181999@gmail.com>
2025-09-07 23:26:36 +0000

    placeholder for low bit files

---
+++ low_bit_inference/torchinductor_fp4.py
@@ -0,0 +1,41 @@
+import os
+import torch
+from torchao.quantization import quantize_, Float8WeightOnlyConfig
+from omegaconf import OmegaConf
+# utils
+from .hf_loader import load_model_tokenizer_prompt_cache
+from .utils.config_utils import get_config
+from .utils.profile_utils import profile_model
+
+
+config = get_config()
+print("config used -- ", OmegaConf.to_yaml(config), sep="\n")
+torch.cuda.set_device(config.device)
+print(f"PyTorch sees {torch.cuda.device_count()} devices, current device: {torch.cuda.current_device()}")
+
+print(f"Loading pretrained model and tokenizer: {config.model_id}.")
+model, tokenizer, prompt, past_key_values = load_model_tokenizer_prompt_cache(config)
+print(f"Model loaded {config.model_id}.")
+
+# compile the model here if you want
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cuda.matmul.allow_bf16_reduced_precision_reduction = True
+torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction = True
+torch.backends.cuda.enable_flash_sdp(True)
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = True
+
+torch._inductor.config.benchmark_kernel = True
+torch._inductor.config.max_autotune = True
+torch._inductor.config.coordinate_descent_tuning = True
+torch._inductor.config.triton.cudagraphs = True
+torch._inductor.config.benchmark_fusion = True
+torch._inductor.config.freezing = True
+
+quantize_(model, Float8WeightOnlyConfig())
+quantize_(past_key_values, Float8WeightOnlyConfig())
+model = model.to(config.device)
+print("Model moved to GPU, starting profiling.")
+
+profile_model(model, tokenizer, past_key_values, prompt, config)
