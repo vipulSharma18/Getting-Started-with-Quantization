2cc03c2e29dbbda3a912a456bbef3a8066d06c72
Vipul Sharma <vipuls181999@gmail.com>
2025-10-24 17:46:27 -0500

    wip

--- low_bit_inference/gemlite_demo.py
+++ low_bit_inference/gemlite_demo.py
@@ -3,78 +3,24 @@ from omegaconf import OmegaConf
 from .hf_loader import load_model_tokenizer_prompt_cache
 from .utils.config_utils import get_config, to_torch_dtype
 from .utils.profile_utils import profile_model
-
 import gemlite
+from gemlite import DType, GemLiteLinear
 from gemlite.core import TORCH_TO_DTYPE
 from gemlite.helper import (
     cleanup_linear,
     patch_model,
-    warmup
+    A16W4_HQQ_INT
 )
+ 
 
-from gemlite.helper import A16W8
-from gemlite import DType, GemLiteLinear
-
+gemlite.reset_cache()
 gemlite.set_autotune("max")  # fast for fast startup, but slow perf. max for slow startup, but best perf.
 
-def autoname_modules(m):
-    for name, module in m.named_modules():
-        module.name = name  
-
-def patch_linearlayers(model, fct):
-    for name, layer in model.named_children():
-        if isinstance(layer, torch.nn.Linear):
-            setattr(model, name, fct(layer, name))
-        else:
-            patch_linearlayers(layer, fct)
-
-def patch_linear_to_gemlite(layer, name):
-    global W_nbits, quantize_activations
-    layer = layer.to(device, non_blocking=True)
-    #if('lm_head' in name): return layer
-
-    weight, bias = layer.weight, layer.bias
-
-    if(isinstance(weight, torch.nn.Parameter)):
-        weight = weight.data
-    if(isinstance(bias, torch.nn.Parameter)):
-        bias = bias.data
-
-    out_features  = weight.shape[0]
-    in_features   = weight.shape[1]
-
-    ############################################################################
-    #Quantize: A8Wn only works with symmetric quant with single shift
+def create_gemlite_processor(W_nbits=4, quantize_activations=False):
     assert W_nbits in [8, 4, 2, 1.58, 1], "Invalid W_nbits."
     nbits_to_max_val = {8:127, 4:7, 2:1, 1.58:1, 1:1}
-    weight  = weight.to(device=device, dtype=compute_dtype)
-    
-    #weight ~ (W_q.float() - zeros) * scales
-    if W_nbits == 1: #BUG in W_nbits==1. with A8 activations add shift only x 2 in new W_group_mode.
-        W_q = weight.sign() #[-1, 1]
-        W_q[W_q == 0] = 1
-        zeros  = 0.50
-        scales = torch.ones((out_features, 1), dtype=compute_dtype, device=device) * weight.abs().max() * 2
-        W_q    = ((W_q + zeros) / 2).to(torch.uint8) #[0, 1]
-    elif W_nbits == 1.58:
-        W_q     = weight.sign() #[-1, 0, 1]
-        zeros   = 1 
-        scales  = torch.ones((out_features, 1), dtype=compute_dtype, device=device) * weight.abs().max()
-        W_q     = (W_q + zeros).to(torch.uint8) #[0, 1, 2]
-    else:
-        max_val = nbits_to_max_val[W_nbits]
-        zeros   = max_val
-        scales  = weight.float().abs().amax(axis=1, keepdim=True) / max_val
-        W_q     = (((weight / scales) + zeros).round_()).to(torch.uint8)
-    ############################################################################
-
-
-    if quantize_activations:
-        input_dtype = DType.INT8
-        scale_activations = True
-    else:
-        input_dtype = TORCH_TO_DTYPE[compute_dtype]
-        scale_activations = False
+    max_val = nbits_to_max_val[W_nbits]
+    zeros   = max_val
 
     gemlite_linear = GemLiteLinear(round(W_nbits), 
                     group_size=in_features, 
@@ -85,7 +31,6 @@ def patch_linear_to_gemlite(layer, name):
                     scaled_activations=scale_activations,
                     )
 
-    bias = bias.clone().to(device=self.device, dtype=compute_dtype) if (bias is not None) else None
 
     gemlite_linear.pack(W_q, scales=scales, zeros=zeros, bias=bias, fma_mode=False)
 
@@ -96,7 +41,6 @@ def patch_linear_to_gemlite(layer, name):
         gemlite_linear.W_group_mode       = 3 #(Wq - zeros) * scales
         gemlite_linear.channel_scale_mode = 0 #no post-scaling
 
-    del weight, W_q, bias
     torch.cuda.empty_cache()
 
     return gemlite_linear
@@ -147,7 +91,6 @@ def model_quantize(causal_model, quantized=False):
     quantize_activations = False
     W_nbits = 1
     # gemlite linear layer replacement with GemLite.linear object
-    autoname_modules(model)
     patch_linearlayers(model, patch_linear_to_gemlite)
 
     if not quantized:
