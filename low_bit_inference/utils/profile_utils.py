e39498a0dcd0e72df31f0fe13b42560c89f6f346
Vipul Sharma <vipuls181999@gmail.com>
2025-10-30 16:40:43 +0000

    more logging' git push

--- low_bit_inference/utils/profile_utils.py
+++ low_bit_inference/utils/profile_utils.py
@@ -54,6 +54,7 @@ def custom_trace_handler(prof, memory_snapshot=None, root_dir='./'):
     root_dir = os.path.abspath(root_dir)
     file_prefix = os.path.join(root_dir, f"{timestamp}")
 
+    print("Profiler exporting started. Path:", file_prefix)
     prof.export_chrome_trace(f"{file_prefix}.json.gz")
     prof.export_memory_timeline(f"{file_prefix}.html")
 
